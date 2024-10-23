26430edc50f6a0aa2fbc993a942271fdf61dd9cc
TEnders64 <tenders64@gmail.com>
2024-10-23 13:51:52 -0700

    fetch inside useEffect & react-bootstrap installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,31 @@
-import './App.css'
+import { useEffect } from "react";
+import "./App.css";
 
 function App() {
-
-    fetch('https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=5&api_key=fz5q52ue7kq3awvrdwkjajd80tn7b2g9182feitdwfturixex')
-    .then(response => {
-      if (!response.ok){
-        throw new Error('Network response not ok');
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log(data.word);
-    })
-    .catch(error => {
-        console.error('There was a problem when fetching:', error);
-    })
-
-
+  useEffect(() => {
+    const API_KEY = 'fz5q52ue7kq3awvrdwkjajd80tn7b2g9182feitdwfturixex';
+    fetch(
+      `https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=5&api_key=${API_KEY}`
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data.word);
+      })
+      .catch((error) => {
+        console.error("There was a problem when fetching:", error);
+      });
+  }, []);
 
   return (
     <>
-        <h1>Vordle</h1>
+      <h1>Vordle</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
